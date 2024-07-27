3ac1959d967b5bb8983a905538ef464b40862967
alexanderpoge <flekssando@mail.ru>
2024-07-27 10:37:33 +0300

    add: finish component navbar

--- src/shared/ActionButton.tsx
+++ src/shared/ActionButton.tsx
@@ -9,7 +9,11 @@ type Props = {
 
 export const ActionButton = ({ children, setSelectedPage }:Props) => {
     return (
-        <AnchorLink>
+        <AnchorLink
+            className='rounded-md bg-secondary-500 px-10 py-2 hover:bg-primary-500 hover:text-white'
+            onClick={() => setSelectedPage(SelectedPage.ContactUs)}
+            href={`#${SelectedPage.ContactUs}`}
+        >
             {children}
         </AnchorLink>
     );
